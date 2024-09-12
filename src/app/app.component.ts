c7d05a457dd025e0ece135a0487d5ae24156ecbb
Aman Saeed <amantatla312@gmail.com>
2024-09-12 14:16:31 +0500

    ngrx basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from "./counter/counter.component";
+import { AppState } from './states/app.state';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectCount } from './states/counter/counter.selector';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  conunt$: Observable<number>;
+  constructor(private store:Store<AppState>) {
+    this.conunt$ = this.store.select(selectCount)
+  }
   title = 'counExample';
 }
